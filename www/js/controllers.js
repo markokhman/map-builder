c659272de9a439026e54b6fa9e6df0ab37c1a0df
Mark Okhman <markokhman1993@gmail.com>
2016-03-19 14:46:34 +0200

    Working button Get my position

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,86 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
- 
+  var options = {timeout: 10000, enableHighAccuracy: true}
+  var faisalabad = {lat:31.4181, lng:73.0776}
+  function addYourLocationButton(map, marker) {
+        var controlDiv = document.createElement('div');
+
+        var firstChild = document.createElement('button');
+        firstChild.style.backgroundColor = '#fff';
+        firstChild.style.border = 'none';
+        firstChild.style.outline = 'none';
+        firstChild.style.width = '28px';
+        firstChild.style.height = '28px';
+        firstChild.style.borderRadius = '2px';
+        firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+        firstChild.style.cursor = 'pointer';
+        firstChild.style.marginRight = '10px';
+        firstChild.style.padding = '0px';
+        firstChild.title = 'Your Location';
+        controlDiv.appendChild(firstChild);
+
+        var secondChild = document.createElement('div');
+        secondChild.style.margin = '5px';
+        secondChild.style.width = '18px';
+        secondChild.style.height = '18px';
+        secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-1x.png)';
+        secondChild.style.backgroundSize = '180px 18px';
+        secondChild.style.backgroundPosition = '0px 0px';
+        secondChild.style.backgroundRepeat = 'no-repeat';
+        secondChild.id = 'you_location_img';
+        firstChild.appendChild(secondChild);
+
+        google.maps.event.addListener(map, 'dragend', function() {
+            $('#you_location_img').css('background-position', '0px 0px');
+        });
+
+        firstChild.addEventListener('click', function() {
+            var imgX = '0';
+            var animationInterval = setInterval(function(){
+                if(imgX == '-18') imgX = '0';
+                else imgX = '-18';
+                $('#you_location_img').css('background-position', imgX+'px 0px');
+            }, 500);
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    marker.setPosition(latlng);
+                    map.setCenter(latlng);
+                    clearInterval(animationInterval);
+                    $('#you_location_img').css('background-position', '-144px 0px');
+                });
+            }
+            else{
+                clearInterval(animationInterval);
+                $('#you_location_img').css('background-position', '0px 0px');
+            }
+        });
+
+        controlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+    }
+     
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
  
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
- 
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+   
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        rotateControl: true,
+        disableDefaultUI: false
+      };
+   
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      // $scope.map.getUiSettings().setMyLocationButtonEnabled(true);
+       var myMarker = new google.maps.Marker({
+          map: map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+      });
+      addYourLocationButton($scope.map, myMarker);
  
   }, function(error){
     console.log("Could not get location");
@@ -29,7 +96,7 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
+  
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
@@ -45,3 +112,4 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 });
+
